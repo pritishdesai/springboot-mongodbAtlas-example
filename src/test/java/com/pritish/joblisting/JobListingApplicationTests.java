86be45ae3594600d1a2209149a8247b68757fb8d
pritishmd <159415077+pritishmd@users.noreply.github.com>
2024-04-03 16:19:34 +0530

    Initial Commit

---
+++ src/test/java/com/pritish/joblisting/JobListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritish.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
